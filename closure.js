d3d79f875f57e836659efb87f70a4a6b8a266514
Charles Gaudreau Jackson <gaudreaujacksoncharles@gmail.com>
2016-04-14 11:35:36 -0400

    array functions

--- closure.js
+++ closure.js
@@ -1,11 +1,23 @@
 /* exposes how functions see variables */
-
 function fun1() {
   var someVar1 = "super secret";
   function fun2() {
     console.log("The value of someVar is: " + someVar1);
-    alert("get");
   }
   return fun2();
 }
 console.log(fun1());
+
+/* other example */
+function adder(firstNumberToBeAdded) {
+  return function(secondNumberToBeAdded) {
+    return firstNumberToBeAdded + secondNumberToBeAdded;
+  }
+}
+var addTen  = adder(10);
+addTen(20);
+addTen(32);
+var increment = adder(1);
+increment(41);
+
+/* closure: a function and an environment (whats the environment?) */
